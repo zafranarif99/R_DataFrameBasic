992602ae3a2f423b72ec0f10b5a0a05eba4460aa
Zafran Arif <zafranarif@gmail.com>
2023-11-01 13:10:50 -0700

    update

--- CreateScatterplot.R
+++ CreateScatterplot.R
@@ -35,6 +35,8 @@ ggplot(data = mpg) +
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy), color = "red")
 
+# We builds a new chart for each level of a categorical variable
+# we use facet_wrap() here
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = cty)) + 
   facet_wrap(~ class, nrow = 2)
@@ -42,6 +44,7 @@ ggplot(data = mpg) +
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = drv, y = cyl, color = class))
 
+# We use facet_grid() here
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_grid(drv ~ .)
@@ -52,7 +55,6 @@ ggplot(data = mpg) +
 
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy))
-
 
 ggplot(data = mpg) + 
   geom_smooth(mapping = aes(x = displ, y = hwy))
